744461e4293c0200a991c39bd26c51d0fb2e1222
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-01-17 17:20:50 -0800

    challenges completed.

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './styles.css';
 
-function Product({ id, name, description, price, category }) {
+function Product({ name, description, price, category, units, rating }) {
   return (
     <div className="Product">
       <h3>{name}</h3>
       <p>{description}</p>
-      <p className="price">${price}</p>
+      <p className="price">Price: ${price}</p>
       <p className="category">Category: {category}</p>
+      <p className="units">Units: {units}</p>
+      <p className="rating">Rating: {rating}</p>
     </div>
   );
 }
 
 export default Product;
+
+
 
